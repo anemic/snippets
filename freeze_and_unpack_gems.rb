5471f231f0590c238b3c4a937ffcc2bd40325a56
Ryan Farrand <ryan@killswitchcollective.com>
2008-10-27 00:51:23 -0500

    updated rake and gem commands for rails 2.x

--- freeze_and_unpack_gems.rb
+++
@@ -1,7 +0,0 @@
-# unpack and build gems rails 2.x
-
-# add to enviornment.rb
-config.gem "hpricot" 
-
-rake gems:unpack GEM=hpricot 
-rake gems:build
---
+++ rake_and_gem_commands.rb
@@ -0,0 +1,28 @@
+# gems and rails 2.x configurations
+
+# in enviornment.rb or for specific environments 
+Rails::Initializer.run do |config| 
+
+  # Require the latest version of haml
+  config.gem "haml"
+
+  # Require a specific version of chronic
+  config.gem "chronic", :version => '0.2.3'
+
+  # Require a gem from a non-standard repo
+  config.gem "hpricot", :source => "http://code.whytheluckystiff.net"
+
+  # Require a gem that needs to require a file different than the gem's name
+  # I.e. if you normally load the gem with require 'aws/s3' instead of
+  # require 'aws-s3' then you would need to specify the :lib option
+  config.gem "aws-s3", :lib => "aws/s3" 
+end
+
+# install all gems spesified in Rails::Initializer
+rake gems:install
+
+# unpack gem to vender/gems
+rake gems:unpack GEM=hpricot 
+
+# builds all gems in vender/gems
+rake gems:build
