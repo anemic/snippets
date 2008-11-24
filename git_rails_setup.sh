b8e54120cddfd1f06524716c2776851bf2880c9d
Ryan Farrand <anemic@me.com>
2008-11-23 22:12:36 -0600

    added working with branch and remote to git_rails_setup quick start guide

--- git_rails_setup.sh
+++ git_rails_setup.sh
@@ -1,3 +1,5 @@
+# this is a quick start for setting up a git repo in a rails project and working with a remote and local branch
+
 # initialize the repo
 git init
 
@@ -21,3 +23,29 @@ git add .
 
 # initial commit
 git commit -a -m "initial import of project"
+
+# add remote
+git remote add origin git@example.com:git_username/projectname.git
+
+# push up to remote
+git push origin master
+
+# create branch for local dev
+git branch my_name
+
+# work in my branch
+git co my_name
+
+# pull in changes from master and apply to my branch
+git co master
+git pull origin master
+git co my_name
+git merge master
+
+# merge in my changes into master when done
+git co master
+git merge my_name
+git push origin master
+
+# pull in changes
+git pull origin branch_name
