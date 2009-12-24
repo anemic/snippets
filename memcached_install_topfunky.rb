84b70264777867482a6e697518e8410a66d9b17f
Ryan Farrand <anemic@gummo.local>
2009-12-24 01:47:44 -0600

    add instructions for install in memcached

--- memcached_install_topfunky.rb
+++
@@ -1,7 +0,0 @@
-# Install Memcached on Mac OS X from http://nubyonrails.com/articles/memcached-basics-for-rails
-# This is basically just a self note but made public just incase some may be helped. So use at your own risk.
-
-download http://topfunky.net/svn/shovel/memcached/install-memcached.sh
-mv install-memcached.sh ~/src/memcached
-cd ~/src/memcached
-sudo sh install-memcached.sh
