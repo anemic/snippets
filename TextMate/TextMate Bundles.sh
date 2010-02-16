24044db1272c30e13ca1eb353386c21b352ddb9c
Ryan <rubydev@me.com>
2010-02-16 01:52:43 -0600

    added some textmate bundles

---
+++ TextMate/TextMate Bundles.sh
@@ -0,0 +1,49 @@
+# TextMate Bundles 
+
+# To install bundels:
+mkdir -p ~/Library/Application\ Support/TextMate/Bundles
+cd ~/Library/Application\ Support/TextMate/Bundles
+git clone git://github.com/user_name/somebundle_tmbundle.git "Some\ Bundle.tmbundle"
+osascript -e 'tell app "TextMate" to reload bundles'
+
+# Bundles
+
+RSpec-tmbundle  					http://github.com/dchelimsky/rspec-tmbundle
+
+git-tmbundle  						http://github.com/timcharper/git-tmbundle
+
+cucumber-tmbundle  					http://github.com/bmabey/cucumber-tmbundle
+
+github-tmbundle 					http://github.com/drnic/github-tmbundle
+
+formtastic_tmbundle 				http://github.com/grimen/formtastic_tmbundle 
+
+handcrafted-haml-textmate-bundle 	http://github.com/handcrafted/handcrafted-haml-textmate-bundle
+
+RubyAMP 							http://github.com/timcharper/rubyamp#readme
+
+merb-tmbundle 						http://github.com/drnic/merb-tmbundle
+
+code-beautifier.tmbundle 			http://github.com/mocoso/code-beautifier.tmbundle
+
+nginx-tmbundle 						http://github.com/johnmuhl/nginx-tmbundle
+
+MacRuby-tmbundle 					http://github.com/drnic/macruby-tmbundle
+
+RSpec-story-tmbundle 				http://github.com/bmabey/rspec-story-tmbundle
+
+Ruby-HAML-tmbundle 					http://github.com/douglasjarquin/ruby-haml-tmbundle
+
+jQuery-tmbundle 					http://github.com/kswedberg/jquery-tmbundle
+
+gists-tmbundle 						http://github.com/ivanvc/gists-tmbundle
+$ git config --global github.user [your username]
+$ git config --global github.token [your token]
+
+Android.tmbundle 					http://github.com/peon/Android.tmbundle
+
+compass_blueprint_tmbundle 			http://github.com/grimen/compass_blueprint_tmbundle
+
+APIDock-tmbundle 					http://github.com/metavida/apidock-tmbundle
+
+
