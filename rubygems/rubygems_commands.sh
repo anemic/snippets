98f974c4d78d2816b73f9903fd3b34459ed00e4c
Ryan Farrand <rubydev@me.com>
2011-05-03 07:19:16 -0500

    added install for postgres and pg gems

--- rubygems/rubygems_commands.sh
+++ rubygems/rubygems_commands.sh
@@ -1,3 +1,11 @@
 # remove all installed gems
 $ gem list | cut -d" " -f1 | xargs gem uninstall -aIx
 
+# install mysql2
+$ env ARCHFLAGS="-arch i386" gem install mysql2 -- --with-mysql-config=/usr/local/mysql/bin/mysql_config
+
+# install postgres
+$ sudo gem install postgres -- --with-pgsql-dir=/Library/PostgreSQL
+
+# install pg
+$ sudo gem install pg -- --with-pg-dir=/Library/PostgreSQL
