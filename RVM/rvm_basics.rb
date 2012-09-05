27fdd3ce85dcad5ebb3ac9a28e0bb98171fddf3e
Ryan <ryan@codejeweler.com>
2012-09-05 14:30:00 -0500

    added an example of a .rvmrc file to change Ruby versions on a per project or per directory basis.

--- RVM/rvm_basics.rb
+++ RVM/rvm_basics.rb
@@ -1,3 +1,7 @@
 # install RVM
 curl -L https://get.rvm.io | bash -s stable --ruby
 
+# to automatically set ruby versions to use based on directory or by git repo
+# create a .rvmrc in the root directory and place the following (modify as needed)
+rvm use 1.9.2@railscasts --create
+
