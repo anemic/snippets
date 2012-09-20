725f2a14269604911ef7eae6b4a0adb7e6cb88bb
Ryan <ryan@codejeweler.com>
2012-09-20 02:31:27 -0500

    added debugging menus for OS X 10.8

---
+++ zsh basics.sh
@@ -0,0 +1,14 @@
+
+# set shell to zsh 
+chsh -s /bin/zsh
+
+# to switch back to bash 
+chsh -s /bin/bash
+
+# oh-my-zsh is a great package of usefull utilites, scripts, themes, and plugins for zsh 
+# install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# see a list of themes as well as previews 
+https://github.com/robbyrussell/oh-my-zsh/wiki/themes
+
